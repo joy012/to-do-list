7d69b0c08d33749dfd46d7a1abdcfd0a4ddc0ff7
joy012 <joy.buet.012@gmail.com>
2020-08-21 15:13:19 +0600

    color added

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById('toDoForm').addEventListener('submit', storeToDo);
 function storeToDo(event){
     const toDoText = document.getElementById('inputVal').value;
     const id = Math.round(Math.random() * 100);
-    const status = 'pending';
+    const checkedStatus = 'false';
 
-    const toDo = {toDoText, id, status};
+    const toDo = {toDoText, id, checkedStatus};
     let toDos = [];
     if(localStorage.getItem('toDos')){
         toDos = JSON.parse(localStorage.getItem('toDos'));
@@ -21,6 +21,13 @@ function storeToDo(event){
     
 }
 
+// const checkboxChecked = id => {
+//     const toDos = JSON.parse(localStorage.getItem('toDos'));
+//     const currentBox = toDos.find(toDo => toDo.id === id);
+//     currentBox.checkedStatus = 'true';
+//     localStorage.setItem('toDos', JSON.stringify(toDos));
+//     showToDo();
+// }
 const deleteToDo = (id) => {
     const toDos = JSON.parse(localStorage.getItem('toDos'));
     const remainingToDo = toDos.filter(toDo => toDo.id !== id);
@@ -34,13 +41,14 @@ const showToDo = () => {
     toDoList.innerHTML = '';
 
     for (let i = 0; i < toDos.length; i++) {
+        const {toDoText, checkedStatus, id} = toDos[i];
         toDoList.innerHTML += 
         `<div id='single-toDo' class="p-3 my-3 col-md-9 mx-auto d-flex justify-content-between">
             <div class="form-check form-check-inline">
-                <input class="form-check-input mx-2" type="checkbox" id="inlineCheckbox1">
-                <label class="form-check-label" for="inlineCheckbox1"><h4 class=" text-white">${toDos[i].toDoText}</h4></label>
+                <input class="form-check-input mx-2" id="${id}" type="checkbox" id="inlineCheckbox1">
+                <label class="form-check-label" for="inlineCheckbox1"><h4 class=" text-white">${toDoText}</h4></label>
             </div>
-            <button onclick="deleteToDo(${toDos[i].id})" class="btn btn-danger rounded-pill">✖</button>
+            <button onclick="deleteToDo(${id})" class="btn btn-danger rounded-pill">✖</button>
         </div>`; 
     }
 }
